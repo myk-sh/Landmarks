06ad20955d1e520edd8286d26d6322aa468f0cbd
myk-sh <mikhail_sh@icloud.com>
2023-07-01 11:00:02 +0700

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Misha Shapkin on 1/7/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
